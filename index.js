60498c033344ed708ae24ca25505579975133255
Andreas Gillström <andreasgillstrom@gmail.com>
2015-09-16 19:55:57 +0200

    Small tweaks

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
 'use strict';
-var osxBluetooth = require('osx-bluetooth');
-
-exports.on = osxBluetooth.on;
-exports.off = osxBluetooth.off;
-exports.toggle = osxBluetooth.toggle;
-exports.isOn = osxBluetooth.isOn;
+module.exports = require('osx-bluetooth');
