cd5ffa2b08a35436adaa73d0b788a1d0d289002b
Kevin Martensson <kevinmartensson@gmail.com>
2015-10-22 10:32:12 +0200

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,23 +1,13 @@
-'use strict';
-var test = require('ava');
-var bluetooth = require('./');
+import test from 'ava';
+import bluetooth from './';
 
 if (!process.env.CI) {
-	test('isOn', function (t) {
-		t.plan(1);
-
-		bluetooth.isOn().then(function (res) {
-			t.assert(typeof res === 'boolean');
-		});
+	test('isOn', async t => {
+		t.is(await bluetooth.isOn(), 'boolean');
 	});
 
-	test('set state to 0', function (t) {
-		t.plan(1);
-
-		bluetooth.off().then(function () {
-			bluetooth.isOn().then(function (res) {
-				t.assert(res === false);
-			});
-		});
+	test('set state to 0', async t => {
+		await bluetooth.off();
+		t.false(await bluetooth.isOn());
 	});
 }
